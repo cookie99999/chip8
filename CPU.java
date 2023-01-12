db809870d1a07a837d88bbabdb83cb277fa4256b
cookie99999 <77994115+cookie99999@users.noreply.github.com>
2023-01-11 22:22:03 -0500

    Initial commit

---
+++ CPU.java
@@ -0,0 +1,75 @@
+public class CPU {
+    private byte[] memory = new byte[4096];
+    
+    private short pc;
+    private short ir;
+    private byte delay_timer;
+    private byte sound_timer;
+    private byte[] registers = new byte[16];
+
+    private boolean isDebug = true;
+
+    public void CPU() {
+	this.pc = 0x200;
+	this.ir = 0;
+	this.delay_timer = (byte)0xff;
+	this.sound_timer = 0;
+	for (byte b : registers)
+	    b = 0;
+	for (byte b : memory)
+	    b = 0;
+    }
+
+    private void debugPrint(short opcode) {
+	System.out.println(String.format("%04x\npc: %04x ir: %04x delay: %02x sound: %02x",
+					 opcode, this.pc, this.ir, this.delay_timer, this.sound_timer));
+	for (byte i = 0; i < 16; i++)
+	    System.out.print(String.format("V%x: %02x ", i, registers[i]));
+	System.out.println();
+    }
+    
+    public void writeMem(short address, byte data) {
+	//check errors
+	this.memory[address] = data;
+    }
+    
+    public void step() {
+	short opcode = (short)((this.memory[this.pc] << 8) | (this.memory[this.pc + 1]));
+	byte op = (byte)((opcode & 0xf000) >>> 12);
+	byte x = (byte)((opcode & 0x0f00) >>> 8);
+	byte y = (byte)((opcode & 0x00f0) >>> 4);
+	byte n = (byte)(opcode & 0x000f);
+	byte nn = (byte)(opcode & 0x00ff);
+	short nnn = (short)(opcode & 0x0fff);
+
+	if (this.isDebug)
+	    debugPrint(opcode);
+
+	this.pc += 2; //increment before execution so jmp won't be messed up
+	
+	switch (op) {
+	case 0x0:
+	    if (opcode == 0x00e0) {
+		System.out.println("<DEBUG> screen cleared");
+	    }
+	    break;
+	case 0x1:
+	    this.pc = nnn;
+	    break;
+	case 0x6:
+	    this.registers[x] = nn;
+	    break;
+	case 0x7:
+	    this.registers[x] += nn;
+	    break;
+	case 0xa:
+	    this.ir = nnn;
+	    break;
+	case 0xd:
+	    System.out.println("<DEBUG> drew " + n + "to " + registers[x] + ", " + registers[y]);
+	    break;
+	default:
+	    System.out.println("<ERROR> unimplemented opcode " + String.format("%04x", opcode));
+	}
+    }
+}
---
+++ Chip8.java
@@ -0,0 +1,43 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+    
+public class Chip8 {
+    public static byte[] loadBin(String path) {
+	Path p = Paths.get(path);
+	byte[] data = null;
+	try {
+	    data = Files.readAllBytes(p);
+	} catch (java.io.IOException e) {
+	    System.out.println("<ERROR> IOException in loadBin");
+	    System.out.println(e);
+	    System.exit(-1);
+	}
+	return data;
+    }
+    
+    public static void main(String[] args) {
+	byte[] bin = null;
+	try {
+	    bin = loadBin(args[0]);
+	} catch (ArrayIndexOutOfBoundsException e) {
+	    System.out.println("Usage: java Chip8 <bin file path>");
+	    System.out.println(e);
+	    System.exit(0);
+	}
+	CPU cpu = new CPU();
+	int binLen = bin.length;
+	if (binLen > 4096 - 0x200) {
+	    System.out.println("<ERROR> input file too large");
+	    System.exit(-1);
+	}
+
+	for (short i = 0; i < binLen; i++) {
+	    cpu.writeMem((short)(0x200 + i), bin[i]);
+	}
+
+	while (true) {
+	    cpu.step();
+	}
+    }
+}
